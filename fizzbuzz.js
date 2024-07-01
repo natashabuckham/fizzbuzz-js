4697c32d8f659eb8c25e901eb552240bd18be134
Natasha Buckham <natasha.buckham@softwire.com>
2024-07-01 15:31:35 +0100

    Stretch Part 1 - Adds Bang for multiples of 7. Plus FizzBang and BuzzBang

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,10 +6,16 @@ function fizzbuzz() {
     for (let i = 1; i < 101; i++) {
         if (i % 15 == 0) {
             console.log("FizzBuzz");
+        } else if (i % 21 == 0) {
+            console.log("FizzBang");
+        } else if (i % 35 == 0) {
+            console.log("BuzzBang");
         } else if (i % 3 == 0) {
             console.log("Fizz");
         } else if (i % 5 == 0) {
             console.log("Buzz");
+        } else if (i % 7 == 0) {
+            console.log("Bang");
         } else {
             console.log(i);
         }  
