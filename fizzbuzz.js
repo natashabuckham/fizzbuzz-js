a1d8d634ed0c6eb46b312cd3397fc2ac9be02b0f
Natasha Buckham <natasha.buckham@softwire.com>
2024-07-01 15:14:08 +0100

    Part 3 - prints Fizz for multiples of 3

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,7 +4,11 @@
 function fizzbuzz() {
     // Put your code here...
     for (let i = 1; i < 101; i++) {
-        console.log(i);
+        if (i % 3 == 0) {
+            console.log("Fizz");
+        } else {
+            console.log(i);
+        }  
     };
 }
 
