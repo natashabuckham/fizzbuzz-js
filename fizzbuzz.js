ab9d96bca60ffa0ce2eead4c5680a6e7158e13a9
Natasha Buckham <natasha.buckham@softwire.com>
2024-07-01 15:53:26 +0100

    Stretch Part 1 - refactor

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,21 +4,40 @@
 function fizzbuzz() {
     // Put your code here...
     for (let i = 1; i < 101; i++) {
-        if (i % 15 == 0) {
-            console.log("FizzBuzz");
-        } else if (i % 21 == 0) {
-            console.log("FizzBang");
-        } else if (i % 35 == 0) {
-            console.log("BuzzBang");
-        } else if (i % 3 == 0) {
-            console.log("Fizz");
-        } else if (i % 5 == 0) {
-            console.log("Buzz");
-        } else if (i % 7 == 0) {
-            console.log("Bang");
-        } else {
+        let output = "";
+        if (i % 3 == 0) {
+            output += "Fizz";
+        };
+        
+        if (i % 5 == 0) {
+            output += "Buzz";
+        };
+
+        if (i % 7 == 0) {
+            output += "Bang";
+        };
+
+        if (output == "") {
             console.log(i);
-        }  
+        } else {
+            console.log(output); 
+        };
+         
+        // if (i % 15 == 0) {
+        //     console.log("FizzBuzz");
+        // } else if (i % 21 == 0) {
+        //     console.log("FizzBang");
+        // } else if (i % 35 == 0) {
+        //     console.log("BuzzBang");
+        // } else if (i % 3 == 0) {
+        //     console.log("Fizz");
+        // } else if (i % 5 == 0) {
+        //     console.log("Buzz");
+        // } else if (i % 7 == 0) {
+        //     console.log("Bang");
+        // } else {
+        //     console.log(i);
+        // }  
     };
 }
 
