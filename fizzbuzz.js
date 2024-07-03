3cf7272006792310affb8a8e83d2464030310cca
Natasha Buckham <natashaisobel@gmail.com>
2024-07-03 09:48:16 +0100

    Stretch Part 2 - Adds Bong

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,20 +1,23 @@
-
-
 // Here, we create our main function.
 function fizzbuzz() {
     // Put your code here...
     for (let i = 1; i < 101; i++) {
         let output = "";
-        if (i % 3 == 0) {
-            output += "Fizz";
-        };
-        
-        if (i % 5 == 0) {
-            output += "Buzz";
-        };
 
-        if (i % 7 == 0) {
-            output += "Bang";
+        if (i % 11 == 0) {
+            output += "Bong";
+        } else {
+            if (i % 3 == 0) {
+                output += "Fizz";
+            };
+            
+            if (i % 5 == 0) {
+                output += "Buzz";
+            };
+    
+            if (i % 7 == 0) {
+                output += "Bang";
+            };
         };
 
         if (output == "") {
@@ -22,24 +25,8 @@ function fizzbuzz() {
         } else {
             console.log(output); 
         };
-         
-        // if (i % 15 == 0) {
-        //     console.log("FizzBuzz");
-        // } else if (i % 21 == 0) {
-        //     console.log("FizzBang");
-        // } else if (i % 35 == 0) {
-        //     console.log("BuzzBang");
-        // } else if (i % 3 == 0) {
-        //     console.log("Fizz");
-        // } else if (i % 5 == 0) {
-        //     console.log("Buzz");
-        // } else if (i % 7 == 0) {
-        //     console.log("Bang");
-        // } else {
-        //     console.log(i);
-        // }  
     };
-}
+};
 
 // Now we run the main function...
 fizzbuzz();
