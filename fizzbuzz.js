c802f1cb172ee68ae0ae4246c8085e131f3127b4
Natasha Buckham <natashaisobel@gmail.com>
2024-07-03 10:09:01 +0100

    Stretch Part 2 - refactor

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,23 +1,23 @@
 // Here, we create our main function.
 function fizzbuzz() {
     // Put your code here...
+
+    
+
     for (let i = 1; i < 101; i++) {
         let output = "";
 
-        if (i % 11 == 0) {
-            output += "Bong";
-        } else {
-            if (i % 3 == 0) {
-                output += "Fizz";
-            };
-            
-            if (i % 5 == 0) {
-                output += "Buzz";
-            };
-    
-            if (i % 7 == 0) {
-                output += "Bang";
-            };
+        // function to check divisor and add to output string
+        const checkDivisor = (i, divisor, word) => {
+            if (i % divisor == 0) {output += word};
+        };
+
+        checkDivisor(i, 11, "Bong");
+
+        if (output == "") {
+            checkDivisor(i, 3, "Fizz");
+            checkDivisor(i, 5, "Buzz");
+            checkDivisor(i, 7, "Bang");
         };
 
         if (output == "") {
